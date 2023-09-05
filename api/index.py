a14c215dfa2d6248fdaee05f4f0189eac07f0c79
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-05 12:49:56 -0400

    add CORS functions

--- api/index.py
+++ api/index.py
@@ -1,19 +1,12 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 import wave
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+CORS(app)
 
 @app.route('/api/process_audio', methods=['POST'])
 def process_audio():
-    if 'audio' not in request.files:
-        return jsonify({'error': 'No audio file provided'}), 400
-
-    audio_file = request.files['audio']
-    if audio_file.filename == '':
-        return jsonify({'error': 'No selected audio file'}), 400
-
     # Do something with the audio file, such as saving it to disk
 
     return jsonify({'message': 'Audio received successfully'})
