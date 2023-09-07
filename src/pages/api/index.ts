3e7b19cdd5012755917695eb7b578161c30fe046
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-07 19:13:05 -0400

    fixing

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,12 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
-type ResponseData = {
-  message: string
-}
- 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
+export default function handler(req, res) {
   res.status(200).json({ message: 'Hello from Next.js!' })
 }
