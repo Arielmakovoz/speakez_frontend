4c614b45ff2273dfea7297b97e78bb2a8f0ef269
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-07 18:59:31 -0400

    learning

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,10 +1,3 @@
-// index.js
-const express = require('express');
-
 export default function handler(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ name: 'John Doe' });
+    res.status(200).json({ text: 'Hello' });
   }
-// Export the Express API
-module.exports = app;
