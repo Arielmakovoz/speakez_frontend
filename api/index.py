7366ff11e8f56252522a66dc7b8795eb2ae82c0b
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-19 21:43:01 -0400

    revert

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from pydub import AudioSegment
 
 app = Flask(__name__)
 
@@ -11,13 +10,8 @@ def upload_audio():
             # Process the audio file as needed (e.g., save it to disk, perform analysis)
             # You can add your audio processing logic here
 
-            # Calculate the duration of the audio in seconds
-            audio = AudioSegment.from_file(audio_file)
-            duration_in_seconds = len(audio) / 1000  # Duration in milliseconds, convert to seconds
-
-            # Send a response to the client with the duration information
-            return jsonify({"message": "Audio file received and processed successfully",
-                            "duration_seconds": duration_in_seconds})
+            # Send a response to the client
+            return jsonify({"message": "Audio file received and processed successfully"})
         else:
             return jsonify({"message": "No audio file received"}), 400
     except Exception as e:
