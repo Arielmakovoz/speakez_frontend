0914e6d4426bd1a79fb648ec6f186df13a6c367d
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-19 21:36:54 -0400

    try this

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import wave
 
 app = Flask(__name__)
 
@@ -10,8 +11,12 @@ def upload_audio():
             # Process the audio file as needed (e.g., save it to disk, perform analysis)
             # You can add your audio processing logic here
 
-            # Send a response to the client
-            return jsonify({"message": "Audio file received and processed successfully"})
+            # Check the duration of the WAV file
+            with wave.open(audio_file, 'rb') as wav:
+                duration_seconds = float(wav.getnframes()) / wav.getframerate()
+
+            # Send a response to the client with the duration information
+            return jsonify({"message": "Audio file received and processed successfully", "duration_seconds": duration_seconds})
         else:
             return jsonify({"message": "No audio file received"}), 400
     except Exception as e:
