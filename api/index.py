7003a7aed20044b3054c77839db3b483d2e6137e
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-24 19:53:09 -0400

    another one

--- api/index.py
+++ api/index.py
@@ -12,17 +12,13 @@ def hardonest_tracker(audio_file):
 
 @app.route('/api/upload-audio', methods=['POST'])
 def upload_audio():
-    try:
-        audio_file = request.files['audio']
-        if audio_file:
-            hardonest_tracker(audio_file)
-            # Process the audio file as needed (e.g., save it to disk, perform analysis)
-            # You can add your audio processing logic here
 
-            # Send a response to the client
-            return jsonify({"message": "Audio file received and processed successfully"})
-        else:
-            return jsonify({"message": "No audio file received"}), 400
-    except Exception as e:
-        return jsonify({"message": f"Error: {str(e)}"}), 500
+    audio_file = request.files['audio']
+    if audio_file:
+        hardonest_tracker(audio_file)
+        # Process the audio file as needed (e.g., save it to disk, perform analysis)
+        # You can add your audio processing logic here
+
+        # Send a response to the client
+        return jsonify({"message": "Audio file received"})
 
