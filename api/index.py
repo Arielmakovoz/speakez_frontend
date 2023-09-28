6c2b27974ccda7f09782de107c56753a26edab3d
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-27 21:06:46 -0400

    try this

--- api/index.py
+++ api/index.py
@@ -6,12 +6,12 @@ def upload_audio():
     try:
         audio_file = request.files['audio']
         if audio_file:
-            
+            audio_file.read()
             # Process the audio file as needed (e.g., save it to disk, perform analysis)
             # You can add your audio processing logic here
 
             # Send a response to the client
-            return jsonify({"message": f" {type(audio_file)} Audio file received and processed successfully"})
+            return jsonify({"message": f" {audio_file.read()} Audio file received and processed successfully"})
         else:
             return jsonify({"message": "No audio file received"}), 400
     except Exception as e:
