21d9d2458d226f184c41d8307ccf0dcea4c9fc25
Arielmakovoz <arielmakovoz@gmail.com>
2023-09-27 20:17:33 -0400

    revert2

--- api/index.py
+++ api/index.py
@@ -5,28 +5,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-def hardonest_tracker(audio_file):
-
-    audio_data, sr_ = librosa.load(audio_file)
-
-    onset_strength_lst = librosa.onset.onset_strength(y=audio_data, sr=sr_)
-
-    onset_frames = librosa.onset.onset_detect(onset_envelope=onset_strength_lst, sr=sr_)
-    times = librosa.times_like(onset_strength_lst, sr=sr_)
-
-    fig, ax = plt.subplots()
-    ax.plot(times, onset_strength_lst, label="Onset Strength")
-    ax.set(title="Onset Strength Graph")
-    ax.vlines(times[onset_frames], 0, onset_strength_lst.max(), color='r', alpha=0.9,
-            linestyle='--', label='Onsets')
-    return(len(onset_frames))
-
 @app.route('/api/upload-audio', methods=['POST'])
 def upload_audio():
     try:
         audio_file = request.files['audio']
         if audio_file:
-            hardonest_tracker(audio_file)
+            
             # Process the audio file as needed (e.g., save it to disk, perform analysis)
             # You can add your audio processing logic here
 
